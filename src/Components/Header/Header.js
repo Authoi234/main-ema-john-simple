bc6005fd42ddcb03f71a57a97f4651b756b5a1c7
Authoi234 <jawadkhan.authoi@gmail.com>
2023-08-01 13:31:18 +0600

    context checked

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,8 @@ import React from 'react';
 import logo from '../../images/Logo.svg';
 import './Header.css'
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/UserContext';
 
 const Header = () => {
-    const {user} = useContext(AuthContext)
     return (
         <nav className='header'>
             <a href="https://ema-john-shopping-by-authoi.netlify.app/"><img src={logo} alt="" /></a>
@@ -17,7 +14,6 @@ const Header = () => {
                 <Link to="/about">about</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Sign up</Link>
-                <span>{user?.email}</span>
             </div>
         </nav>
     );
